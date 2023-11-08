d76d51d6e75d3e9de4ffdefcf56caea7420de392
“nabeel529” <olusekunn@gmail.com>
2023-11-08 19:33:26 +0000

    Board print inspired

---
+++ BoardPrint.java
@@ -0,0 +1,11 @@
+
+public class BoardPrint{
+        public static void main(String[] args) {
+            System.out.println("|---------------------------------------------------------------------|---------------|");
+            System.out.println("| " + "13   14   15   16   17   18" + " | " + "B2" + " | " + "19   20   21   22   23   24" + " | " + "E1" + " |   ");
+            System.out.print("| " + "12   11   10   09   08   07");
+            System.out.print(" | " + "B2" + " | ");
+            System.out.print("06   05   04   03   02   01" + " | " + "E1" + " |");
+        }
+
+}
