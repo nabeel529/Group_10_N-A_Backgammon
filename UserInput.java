d7d68f21274e0df4f01bf82862a230cfa4f3a449
alexb-25 <a13x.8rady@gmail.com>
2023-11-16 19:48:08 +0000

    added field for help command print method for help as well also a validity checker for the startup will be modified again soon

--- UserInput.java
+++ UserInput.java
@@ -2,18 +2,21 @@ public class UserInput {
     private enum Commands {
         Roll,
         Quit,
+        Help,
         Over;
     }
 
     private Commands command;
 
-    public UserInput(String input) {
+    public void set_Input(String input) {
         input = input.trim().toUpperCase();
 
         if (input.equals("Q")) {
             command = Commands.Quit;
         } else if (input.equals("R")) {
             command = Commands.Roll;
+        } else if (input.equals("H")) {
+            command = Commands.Help;
         } else {
             System.out.print("\nI assume youre testing\n\n");
         }
@@ -29,5 +32,25 @@ public class UserInput {
 
     public Boolean isOver() {
         return command == Commands.Over;
+    }
+
+    public Boolean isHelp() {
+        return command == Commands.Help;
+    }
+
+    public Boolean isValid_start(String input) {
+        input = input.trim().toUpperCase();
+        return (input.equals("Q") || input.equals("R") || input.equals("H"));
+    }
+
+    public void print_start_commands() {
+        System.out.println("List of commands: To roll - R/r");
+        System.out.println("                  To quit - Q/q");
+    }
+
+    public void print_commands() {
+        System.out.println("List of commands: To roll  - R/r");
+        System.out.println("                  To quit  - Q/q");
+        System.out.println("                  For help - H/h");
     }
 }
