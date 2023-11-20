5876659503759a7f98e50919bb71eb04b05ed471
alexb-25 <a13x.8rady@gmail.com>
2023-11-20 21:19:30 +0000

    literally just basic formatting nothing major

--- Die.java
+++ Die.java
@@ -1,101 +1,85 @@
 import java.util.Random;
-public class Die
-{
+
+public class Die {
     private Show show;
     private int[] dieNum;
     private int[] moves;
     private int numMoves;
     private Random random;
 
-    Die()
-    {
+    Die() {
         this.dieNum = new int[2];
         random = new Random();
         this.moves = new int[2];
         show = new Show();
 
         numMoves = 2;
-        dieNum[1] = random.nextInt(1,7);
-        dieNum[0] = random.nextInt(1,7);
+        dieNum[1] = random.nextInt(1, 7);
+        dieNum[0] = random.nextInt(1, 7);
         moves[1] = 1;
         moves[0] = 1;
 
-        if(dieNum[1] == dieNum[0])
-        {
+        if (dieNum[1] == dieNum[0]) {
             numMoves = 4;
             moves[1] = 0;
             moves[0] = 4;
         }
     }
 
-    public int retrieveNumMoves()
-    {
+    public int retrieveNumMoves() {
         return numMoves;
     }
 
-
-    public int retrieveMoves(int num)
-    {
-        if (num == 1)
-        {
+    public int retrieveMoves(int num) {
+        if (num == 1) {
             return moves[0];
         }
 
-        else if (num == 2)
-        {
+        else if (num == 2) {
             return moves[1];
         }
 
-        else
-        {
+        else {
             return 0;
         }
     }
 
-    public int retrieveDieNum(int num)
-    {
-        if (num == 1)
-        {
+    public int retrieveDieNum(int num) {
+        if (num == 1) {
             return dieNum[0];
         }
 
-        else if (num == 2)
-        {
+        else if (num == 2) {
             return dieNum[1];
         }
 
-        else
-        {
+        else {
             return 0;
         }
     }
 
-    public void throwDie()
-    {
+    public void throwDie() {
         numMoves = 2;
-        dieNum[1] = random.nextInt(1,7);
-        dieNum[0] = random.nextInt(1,7);
+        dieNum[1] = random.nextInt(1, 7);
+        dieNum[0] = random.nextInt(1, 7);
         moves[1] = 1;
         moves[0] = 1;
 
-        if(dieNum[1] == dieNum[0])
-        {
+        if (dieNum[1] == dieNum[0]) {
             numMoves = 4;
             moves[1] = 0;
             moves[0] = 4;
         }
     }
 
-    public void makeDieNum(int dieNumOne, int dieNumTwo)
-    {
+    public void makeDieNum(int dieNumOne, int dieNumTwo) {
         numMoves = 2;
         moves[1] = 1;
         moves[0] = 1;
         dieNum[1] = dieNumTwo;
         dieNum[0] = dieNumOne;
 
-        if(dieNum[1] == dieNum[0])
-        {
+        if (dieNum[1] == dieNum[0]) {
             numMoves = 4;
             moves[1] = 0;
             moves[0] = 4;
@@ -104,28 +88,23 @@ public class Die
         show.revealDie(dieNumOne, dieNumTwo);
     }
 
-    public void minusMoves(int num)
-    {
-        if (num == 1)
-        {
+    public void minusMoves(int num) {
+        if (num == 1) {
             moves[0]--;
         }
 
-        else if (num == 2)
-        {
+        else if (num == 2) {
             moves[1]--;
         }
     }
 
-    public void makeMoves(int movesOne, int movesTwo)
-    {
+    public void makeMoves(int movesOne, int movesTwo) {
         moves[1] = movesTwo;
         moves[0] = movesOne;
         numMoves = moves[1] + moves[0];
     }
 
-    public void Nothing()
-    {
+    public void Nothing() {
         numMoves = 0;
         moves[1] = 0;
         moves[0] = 0;
